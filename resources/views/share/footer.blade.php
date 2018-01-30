05fa93ca562fa429942d5efac7013786245d63a2
yjw1029 <1055863721@qq.com>
2018-01-30 21:22:28 +0800

    chang footer

--- resources/views/share/footer.blade.php
+++ resources/views/share/footer.blade.php
@@ -1,8 +1,9 @@
-<footer align="center" style="margin-top:80px;font-family:chinese2">
-	<hr/>
-	<ul style="list-style-type: none">
+<div align="center" style="position:relative;bottom:-230px;;margin-top:80px;font-family:chinese2;color:aliceblue;background:black;width:100%">
+	<br/><br/>
+	<ul style="list-style-type: none;">
 		<li>地址：安徽省合肥市金寨路 96 号，邮政编码：230026 </li>
 		<li>Copyright © (待填) All Rights Reserved. 备案号:(待填)</li>
 		<li>联系我们:(待填)</li>
 	</ul>
-</footer>
+	<br/>
+</div>
