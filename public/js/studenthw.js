1ddcbe2f150f47b0f71f43f795b09108bffd649a
yjw1029 <1055863721@qq.com>
2018-02-27 00:07:35 +0800

    change

---
+++ public/js/studenthw.js
@@ -0,0 +1,34 @@
+$("document").ready(function(){
+	//hover
+	
+	$(".hm-btn-hover1").mouseover(function(){
+		$(this).parents(".hm-btn").css("background-color","#D6D4D4");
+	});
+	
+	$(".hm-btn-hover1").mouseleave(function(){
+		$(this).parents(".hm-btn").css("background-color","#FFFFFF");
+	});
+	
+	var width=$(".hm-btn-sign").css("width");
+	$(".hm-btn-sign").css("height",width);
+	var WidthPart=width.slice(0,width.length-2);
+	var WidthNum=parseInt(WidthPart);
+	$(".hm-btn-body").css("height",WidthNum+5+"px");
+	var btnPadding=WidthNum/2-24;
+	var bodyPadding=WidthNum/5;
+	$(".hm-btn-sign").css("padding-top",btnPadding+"px");
+	$(".hm-btn-body").css("padding-top",bodyPadding+"px");
+	
+	$(window).resize(function(){
+		var Width=$(".hm-btn-sign").css("width");
+		$(".hm-btn-sign").css("height",Width);
+		var WidthPart=Width.slice(0,Width.length-2);
+		var WidthNum=parseInt(WidthPart);
+		$(".hm-btn-body").css("height",WidthNum+5+"px");
+		var btnPadding=WidthNum/2-24;
+		var bodyPadding=WidthNum/5;
+		$(".hm-btn-sign").css("padding-top",btnPadding+"px");
+		$(".hm-btn-body").css("padding-top",bodyPadding+"px");
+	});
+});
+
